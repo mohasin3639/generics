660143b1206ec08a571ee9aa2eea968c24c1661c
mohasin3639 <mohasinsayyad3639@gmail.com>
2023-01-25 11:30:29 +0530

    ADDED:A Welcome Message

---
+++ src/com/bridgelabz/generics/Generics.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.generics;
+
+public class Generics {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Generics practice problem");
+    }
+}
